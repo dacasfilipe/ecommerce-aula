c409dae054bda38a9a5010a79952d558968fbb73
Filipe <filiperdacas@gmail.com>
2025-03-17 11:56:01 -0300

    Update CORS configuration to allow requests from localhost:3000

--- src/main/java/com/senai/ecommerce/config/WebConfig.java
+++ src/main/java/com/senai/ecommerce/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://127.0.0.1:5501", "http://localhost:5500")
+                .allowedOrigins("http://127.0.0.1:5501", "http://localhost:5500", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
